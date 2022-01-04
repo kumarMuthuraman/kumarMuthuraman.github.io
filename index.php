51ace7009d3a2c016666dbce8c75279c5bec77a4
KumarMuthuraman <Kumar.Muthuraman@gmail.com>
2022-01-04 17:28:23 -0600

    Index.php

---
+++ index.php
@@ -0,0 +1,421 @@
+﻿
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39446682-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Kumar Muthuraman's Home Page</title>
+<style type="text/css">
+<!--
+.publist {
+	padding-top: 15px;
+	font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
+}
+.publistArea {
+	list-style-type: decimal;
+	padding-top: 0 px;
+	font-size:18px;
+	font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
+}
+.lead em {
+	font-family: Palatino Linotype, Book Antiqua, Palatino, serif;
+}
+.Photo {
+	padding-left: 10px;
+}
+-->
+</style></head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+ 
+
+
+<!--HEADER-->
+
+ <tr>
+    <td width="25%" height="225"><img src="files/ko.jpg"  height="225" class="Photo" /></td>
+    <td width="75%" valign="top">
+
+<img align=right  valign="top" src="files/UTlogo.png" width="271" height="53"></img><br><br><br><br>
+
+      <hr size="3" noshade="noshade" />
+      <h2>Kumar Muthuraman</h2>
+      <h3>
+      H. Timothy (Tim) Harkins Centennial Professor,<br />
+      University Distinguished Teaching Professor,<br>
+	  Faculty Director, Center for Analytics and Transformative Technologies.<br>
+	  
+<br>
+    GSB 5.156,<br> 
+	Dept. of Information, Risk, and Operations Mgmt. and <br>
+	Dept. of Finance</h3>
+
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td align="right"><h4>(512) 232 3732<br />
+          kumar.muthuraman <u>at</u> mccombs.utexas.edu</h4></td>
+        </tr>
+	
+		 
+
+	
+
+
+      </table>
+    <p>&nbsp;</p></td>
+  </tr>
+
+
+<!--EDUCATION-->
+
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+    <h3>Education</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <p class="lead"><em>Ph.D. (2003), Stanford 
+        University, CA.</em></p>
+      <p class="lead"><em>M.S. (2000), Stanford 
+        University, CA.</em></p>
+      <p class="lead"><em>B.Tech. (1998), Central Electrochemical Research 
+    Institute, Karaikudi, India.</em></p></td>
+  </tr>
+
+<!--Students-->
+
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+    <h3>Ph.D. Students</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <p class="lead">Long Zhao,<em> NUS Business School, Singapore</em></p>
+	<p class="lead">Vishwakant Malladi,<em> Indian School of Business, India</em></p>	  
+	  <p class="lead">Dongyang (Ester) Wang,<em> JD.com Research</em></p>
+<p class="lead">Dan Mitchell, <em>Assistant
+Prof., Industrial & Systems Engineering, University of Minnesota.</em></p>
+
+<!--a TARGET="_top" href=http://weatherhead.case.edu/faculty/Qi-Wu/>-->
+
+      <p class="lead">Qi Wu, <em>Assistant Prof., Weatherhead School of Mgmt, Case Western Univ.</em></p>
+      <p class="lead">Arun Chockalingam, <em>Assistant Prof., School of Industrial Engg., Eindhoven Univ. of Technology, Netherlands</em></p>
+      <p class="lead">Haolin Feng, <em>Associate Prof., Dept of Logistics Engg and Management, Lingnan (University) College, Sun Yat-sen Univ., China
+	
+	
+</em></p>
+      <p class="lead">Tarik Aouam, <em>Associate Prof., Dept of Management Information Science and Operation Management, Ghernt University, Belgium</em></p>
+
+
+  </tr>
+
+<!--Book/Monograph-->
+
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+    <h3>Book</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <p class="lead"><a TARGET="_top" href=files/0900000006.pdf>Solving free-boundary problems with applications in finance</a>, <a TARGET="_top" href=http://www.amazon.com/Free-Boundary-Problems-Applications-Foundations-Stochastic/dp/1601981686>Amazon link</a></p>
+
+
+
+
+
+  </tr>
+
+
+<!--DOWNLOADABLE PAPERS-->
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+      <h3>Selected downloadable papers</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <ul>
+<br><br>
+		<strong class="publistArea">Machine Leanrning and Data-driven Optimization</strong>
+    <li class="publist">L. Zhao, D. Chakrabarti and K. Muthuraman, “<em>Unified Classical and Robust Optimization for Least Squares</em>”,  submitted to <strong>Management Science</strong>, 2020.</em>
+		<a TARGET="_top" href=https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3182422>link</a>
+		</li>
+
+    <li class="publist">L. Zhao, D. Chakrabarti and K. Muthuraman, “<em>Portfolio Construction by Mitigating Error Amplification: The Bounded-Noise Portfolio</em>”,  forthcomming in <strong>Operations Research</strong>, 2018. </em>
+		<a TARGET="_top" href=https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2999407>link</a>
+		</li>
+
+
+
+		<br><br>
+		<strong class="publistArea">Quantitative Finance/Financial Engineering</strong>
+    
+		
+		
+		<li class="publist">D. Mitchell, H. Feng and K. Muthuraman, “<em>Impulse Control of Interest Rates</em>”,  <strong>Operations Research</strong> 62(3), 602–615,2014</em>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=2212478>link</a>
+		</li>
+		
+
+		<li class="publist">A. Chockalingam and K. Muthuraman, “<em>American Options Under Stochastic Volatility</em>”, <strong>Operations Research</strong> 59(4), 793-809, 2011.</em>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1066581>link</a>
+		</li>
+
+		<li class="publist">D. Mitchell, J. Goodman and K. Muthuraman, “<em>Boundary Evolution Equations for American Options</em>”, <strong>Mathematical Finance</strong>.</em>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1966726>link</a>
+		</li>
+
+		<li class="publist"><span class="publist">H. Feng and K. Muthuraman, “<em>A Computational Method for Stochastic Impulse Control Problems</em>”, <strong>Mathematics of Operations Research</strong> 35(4), 2010.</em></span>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1375699>link</a>
+		</li>
+		
+		
+		<li class="publist"><span class="publist">K. Muthuraman, “<em>A moving boundary approach to American option pricing</em>”,<strong> Journal of Economic Dynamics and Control</strong>, 32(11), 3520-3537, 2008. </em></span>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=910882>link</a>
+		</li> 
+
+		<li class="publist"><span class="publist">K. Muthuraman and H. Zha, “<em>Simulation based portfolio optimization for large portfolios with transaction costs</em>”,<strong> Mathematical Finance</strong>, 18(1), 115-134, 2008. </em></span>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=861264>link</a>
+		</li>
+		
+		<li class="publist"><span class="publist">K. Muthuraman and S. Kumar, “<em>Multi-dimensional portfolio optimization with proportional transaction costs</em>”, <strong>Mathematical Finance</strong>, 16(2), 301-335, 2006. </em></span>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=563944>link</a>
+		</li>
+		
+		
+		
+		
+		
+<br><br>
+		<strong class="publistArea">Operations Management</strong>
+    
+    
+    
+
+		<li class="publist">
+		Q. Wu, K. Muthuraman and S. Seshadri, “<em>Inventory Management with Financing Costs and Constraints</em>”, in revision for <strong>Production and Operations Management</strong></em>
+		<!--<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1066581>link</a>-->
+		</li>
+
+		
+		
+		<li class="publist">
+		K. Muthuraman, S. Seshadri and Q. Wu, “<em>Inventory management with Stochastic Lead Times</em>”, <strong>Mathematics of Operations Research</strong></em>
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=2366053>link</a>
+		</li>
+	
+	
+
+		<li class="publist">H. Feng, K. Muthuraman and V. Deshpande, “<em>The Impact of Demand Correlation on Replenishment Policies for Multi-Product Stochastic Inventory Systems with Joint-Replenishment Costs</em>”, <strong>Production and Operations Management</strong>.
+		<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=991356>link</a>
+		</li>
+
+		<li class="publist">S. Kumar and K. Muthuraman, “<em>A numerical method for solving singular stochastic control problems</em>”, <strong>Operations Research</strong>, 52(4), 563-582, 2004.
+		<a TARGET="_top" href=http://www.jstor.org/stable/30036608>link</a>
+		</li>
+
+		<br><br>
+		<strong class="publistArea">HealthCare</strong>
+		
+
+    <li class="publist">
+		K. Muthuraman, D. Wang and D. Morrice“<em>Coordinated Patient Appointment Scheduling for a Multi- Station Healthcare Network</em>”, forthcomming in <strong>Operations Research</strong></em>
+		<!--a TARGET="_top" href=http://onlinelibrary.wiley.com/doi/10.1111/poms.12800/full>link</a-->
+    </li>
+    
+    <li class="publist">
+		D. Wang, D. Morrice, K. Muthuraman, J. Bard, L. Leykum, S. Noorily “<em>Coordinated Scheduling for a Multi-server Network in Outpatient Pre-operative Care</em>”, <strong>Production and Operations Management</strong>, 27(3), 2018.</em>
+		<a TARGET="_top" href=http://onlinelibrary.wiley.com/doi/10.1111/poms.12800/full>link</a>
+		</li>
+
+
+			<li class="publist">
+			S. Chakraborty, K. Muthuraman and M. Lawley, “<em>Sequential clinical scheduling with patient no-shows and general service time distributions</em>”, <strong>IIE Transactions</strong>, 42(5), 354-366, 2010. Best paper in Operations Engineering and Analysis award.</em>
+			<!--<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1066581>link</a>-->
+			</li>
+			
+				<li class="publist">
+				K. Muthuraman and M. A. Lawley, “<em>Stochastic overbooking model for outpatient clinical scheduling with no-shows</em>”, <strong>IIE Transactions</strong>, 40(9), 2008. Among the Top 10 most cited articles of 2008-2010 in IIE Transactions.</em>
+				<!--<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1066581>link</a>-->
+				</li>
+				
+					<li class="publist">
+					F. Lin, K. Muthuraman and M. Lawley, “<em>	An optimal control theory approach to
+						non-pharmaceutical interventions</em>”, <strong>BMC Infectious Diseases</strong>, 10(32), 2010</em>
+					<!--<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1066581>link</a>-->
+					</li>
+		
+				<br><br>
+				<strong class="publistArea">Other</strong> 
+				
+				<li class="publist"><span class="publist">T. Aouam, R. Rardin and K. Muthuraman, “<em>Robust optimization policy benchmarks and modeling errors in natural gas</em>”, <strong>European Journal of Operations Research</strong>, 240(2), 431-438. 2016</span>
+				<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=861341>link</a>
+				</li>
+				
+				
+				<li class="publist"><span class="publist">D. Mitchell, P. Brockett, R. Mendoza and K. Muthuraman, “<em>Modeling and Forecasting Mortality Rates</em>”, <strong>Insurance: Mathematics and Economics</strong> 52 (2), 2013.</span>
+				<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=1966712>link</a>
+				</li>
+			
+<li class="publist"><span class="publist">K. Muthuraman, T. Aouam and R. Rardin, “<em>Regulation of natural gas distribution using policy benchmarks</em>”, <strong>Operations Research</strong>, 56(5), 1131-1145. 2008</span>
+<a TARGET="_top" href=http://papers.ssrn.com/sol3/papers.cfm?abstract_id=861341>link</a>
+</li>
+
+
+
+
+
+
+
+
+<!-- To add: FOT, ORL Jump, OR, Energy Econ, PO JEDC -->
+
+
+
+
+      </ul></td>
+  </tr>
+
+
+
+<!--WORKING/SUBMITTED PAPERS-->
+<!--
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+      <h3>Working and submitted papers</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <ol>
+        <li class="publist">A. Chockalingam and K. Muthuraman, “<em>American Options Under Stochastic Volatility</em>”, in revision for <strong>Operations Research</strong>.</em></li>
+        <li class="publist">F. Lin, K. Muthuraman and M. Lawley, “Optimal Implementation of Non-Pharmaceutical Interventions during Pandemic Influenza”, submitted. </em></li>
+        <li class="publist"> A. Chockalingam and K. Muthuraman, “An Approximate Moving Boundary Method for American Options”, submitted. </em></li>
+        <li class="publist">A. Turkcan, B. Zeng, M. Lawley, K. Muthuraman, “Sequential Clinical Scheduling with Multiple Moment-based Criteria”, submitted. </em></li>
+        <li class="publist">S. Chakraborty, K. Muthuraman and M. Lawley, “Sequential clinical scheduling with patient no-shows and general service time distributions”, submitted. <br />
+          </em></li>
+        <li class="publist">V. Parmeshwaran and K. Muthuraman, “<em>FTR option formulation and pricing</em>”, revised for <strong>Electric Power Systems Review</strong>. </em></li>
+        <li class="publist">A. Chockalingam and K. Muthuraman, “<em>Pricing American options when asset prices jump</em>”, revised for <strong>Operations Research Letters</strong>. </em></li>
+        <li class="publist">H. Feng, K. Muthuraman and V. Deshpande, “<em>Replensihment Policies for Multi-Product Stochastic Inventory Systems with Correlated Demand and Joint-Replenishment Costs</em>”, revised for <strong>Operations Research</strong>. </em></li>
+      </ol></td>
+  </tr>
+-->
+
+
+<!--PUBLISHED/ACCEPTED PAPERS-->
+<!--
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+      <h3>Published and accepted papers</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <ol>
+        <li class="publist"><span class="publist">D. Gotham, K. Muthuraman, P. Preckel, R. Rardin, S. Ruangpattana, “<em>A load factor based mean-variance analysis for fuel diversiﬁcation</em>”, <strong>Energy Econmoics</strong>, In print. </em></span></li>
+        <li class="publist"><span class="publist">K. Muthuraman and S. Kumar, “<em>Solving free-boundary problems with applications in finance</em>”, <strong>Foundations and Trends in Stochastic Systems</strong>, 1(4), 259-341. 2008. This paper is also published as a book. </em></span></li>
+        <li class="publist"><span class="publist">K. Muthuraman, T. Aouam and R. Rardin, “Regulation of natural gas distribution using policy benchmarks”, <strong>Operations Research</strong>, In print. </em></span></li>
+        <li class="publist"><span class="publist">K. Muthuraman, “A moving boundary approach to American option pricing”,<strong> Journal of Economic Dynamics and Control</strong>, 32(11), 3520-3537, 2008. </em></span></li>
+        <li class="publist"><span class="publist">K. Muthuraman and M. A. Lawley, “Stochastic overbooking model for outpatient clinical scheduling with no-shows”,<strong> IIE Transactions</strong>, 40(9), 2008. </em></span></li>
+        <li class="publist"><span class="publist">K. Muthuraman and H. Zha, “Simulation based portfolio optimization for large portfolios with transaction costs”,<strong> Mathematical Finance</strong>, 18(1), 115-134, 2008. </em></span></li>
+        <li class="publist"><span class="publist">K. Muthuraman, “A computational scheme for optimal investment-consumption with proportional transaction costs”,<strong> Journal of Economic Dynamics and Control</strong>, 31(4), 1132-1159, 2007. </em></span></li>
+        <li class="publist"><span class="publist">Kopach, R., P-C. DeLaurentis, M. Lawley, K. Muthuraman, L. Ozsen, R. Rardin, H. Wan, P. Intrevado, X. Qu, and D. Willis, “Effects of Clinical Characteristics on Successful Open Access Scheduling,” <strong>Healthcare Management Science</strong>, 10(2), 111-125, 2007. </em></span></li>
+        <li class="publist"><span class="publist">K. Muthuraman and S. Kumar, “Multi-dimensional portfolio optimization with proportional transaction costs,” <strong>Mathematical Finance</strong>, 16(2), 301-335, 2006. </em></span></li>
+        <li class="publist"><span class="publist">S. Kumar, and K. Muthuraman, “A numerical method for solving singular stochastic control problems”, <strong>Operations Research</strong>, 52(4), 563-582, 2004. </em></span></li>
+        <li class="publist"><span class="publist">V. S. Ramanan, M. Muthukumar, S. Gnanasekaran, M. J. R. Venkataramana and B. Emmanuel, “Green's functions for the Laplace equation in a 3-layer medium, boundary element integrals and their application to cathodic protection”, <strong>Engineering Analysis with Boundary Elements</strong>, 23(9), 777-786, 1999. </span></li>
+        <li class="publist"><span class="publist">S. K. Rangarajan, V. Yegnanarayanan, and M. Muthukumar, “Current losses in a bipolar cell - II: An analysis of the Butler-Volmer regime”, <strong>Electrochimica Acta</strong>, 44(2), 491-502, 1998. </em></span></li>
+      </ol></td>
+  </tr>
+-->
+
+<!--BOOKS-->
+<!--
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+      <h3>Book</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <ol>
+        <li class="publist">K. Muthuraman and S. Kumar, “Solving free-boundary problems with applications in finance”, Series: Foudations and Trends in Stochastic Systems, Now Publishers, 2008.</li>
+      </ol></td>
+  </tr>
+-->
+
+<!--Outside
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+      <h3>External Board Memberships</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <ol>
+        <a href=files/Kumar-WebCV.pdf>Link</a>
+      </ol></td>
+  </tr>
+
+
+  <tr valign="top">
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+
+-->
+
+<!--Other Aff-->
+
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+    <h3>Other Affiliations</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <p class="lead"><em>Founder and Board Member:</em><br>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a target=_top href=http://getfriday.com>GetFriday.com</a> <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a  target=_top href=http://homeShikhari.com>HomeShikari.com</a> <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a target=_top href=http://yourManInIndia.com>YourManInIndia.com</a> <br />
+		 
+</p>
+
+<!--<p class="lead"><em>Consultant:</em><br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a target=_top href=http://www.highfieldscapital.com>Highfields Capital Management</a>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a target=_top href=http://www.aliya.com>Aliya.com</a><br />
+	
+</p>-->
+
+
+  </tr>
+
+
+
+
+<!--CV-->
+  <tr valign="top">
+    <td><hr size="3" noshade="noshade" />
+      <h3>Complete CV</h3></td>
+    <td><hr size="1" noshade="noshade" />
+      <ol>
+        <a href=files/Kumar-WebCV.pdf>Link</a>
+      </ol></td>
+  </tr>
+
+
+  <tr valign="top">
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
